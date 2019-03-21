751d352c86105cdf1d4678780b1287a52bfcb019
Sohairu <sohail428@gmail.com>
2019-03-21 02:38:46 +0200

    Highlighting

--- phantom.py
+++ phantom.py
@@ -1,9 +1,10 @@
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QFileDialog
-from PyQt5.QtGui import QPixmap, QPainter, QColor
+from PyQt5.QtGui import QPixmap, QPainter, QColor, QImage
 from gui import Ui_MainWindow
 import sys
+import math
 
 
 class ApplicationWindow(QtWidgets.QMainWindow):
@@ -28,14 +29,36 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         #self.resize(700,700)
         fileName, _filter = QFileDialog.getOpenFileName(self, "Open", "", "Filter -- All Files (*);;Python Files (*.py)")
         if fileName:
-            self.pixmap = QPixmap(fileName)
+            try:
+                self.highlight.end()
+            except:
+                pass
+            self.img = QImage(fileName)
+            self.pixmap = QPixmap(self.img)
             self.pixmap = self.pixmap.scaled(int(128), int(128), QtCore.Qt.KeepAspectRatio)
+            self.ui.lblPhantom.setPixmap(self.pixmap)
             self.highlight = QPainter()
             self.highlight.begin(self.pixmap)
-            self.highlight.setCompositionMode(QPainter.CompositionMode_ColorDodge);
-            self.highlight.fillRect(0,0,100,100, QColor(180, 180, 180, 100));
-            self.ui.lblPhantom.setPixmap(self.pixmap)
+            self.position = [[0]*2]*5
+            self.number = 0
+            self.ui.lblPhantom.mousePressEvent = self.getPos
+
+    def getPos(self , event):
+        x = math.floor(event.pos().x()*128/self.ui.lblPhantom.frameGeometry().width())
+        y = math.floor(event.pos().y()*128/self.ui.lblPhantom.frameGeometry().height())
+        color = [QColor(0, 0, 255, 75), QColor(0, 255, 0, 75), QColor(255, 0, 0, 75), QColor(255, 0, 255, 75), QColor(255, 255, 0, 75)]
+        if self.number%5==0:
+            print('1')
             self.highlight.end()
+            self.ui.lblPhantom.clear()
+            self.pixmap = QPixmap(self.img)
+            self.pixmap = self.pixmap.scaled(int(128), int(128), QtCore.Qt.KeepAspectRatio)
+            self.highlight.begin(self.pixmap)
+        self.position[self.number%5]=[x,y]
+        print(x,'x',y)
+        self.highlight.fillRect(self.position[self.number%5][0],self.position[self.number%5][1],10,10, color[self.number%5])
+        self.ui.lblPhantom.setPixmap(self.pixmap)
+        self.number += 1
 
     def winResize(self):
         #print('resize');

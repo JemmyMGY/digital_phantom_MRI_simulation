fab705ffc46ac55d22419e97ccb4cccd7620fead
Mohamed Gamal Youssef <you@example.com>
2019-03-27 17:17:53 +0200

    images

--- generation.py
+++ generation.py
@@ -1,91 +1,35 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def create_phantom1(height, width):
-    image = np.zeros((height, width),dtype=np.uint8)
-    image[height//15:height//6, width//3:width//1] = 90
-    image[height//2:height//1, width//2:width//1] = 150
-    image[height//3:height//1, width//6:width//3] = 255
-    return image
+    in_image = np.zeros((height, width),dtype=np.uint8)
+    in_image[height//15:height//6, width//3:width//1] = 90
+    in_image[height//2:height//1, width//2:width//1] = 150
+    in_image[height//3:height//1, width//6:width//3] = 255
+    return in_image
 
 def create_phantom2(height, width):
-    image2 = np.zeros((height, width),dtype=np.uint8)
-    image2[height // 4:height// 2, width // 9:width // 3] = 255
-    image2[height // 2:height// 1, width // 2:width // 1] = 150
-    return image2
+    in_image= np.zeros((height, width),dtype=np.uint8)
+    in_image[height // 4:height// 2, width // 9:width // 3] = 255
+    in_image[height // 2:height// 1, width // 2:width // 1] = 150
+    return in_image
 
 def create_phantom3(height, width):
-    image3 = np.zeros((height, width),dtype=np.uint8)
-    image3[height // 4:height // 2, width // 9:width // 3] = 150
-    image3[height// 2:height // 1, width // 3:width // 2] = 255
-    image3[height // 13:height // 5, width // 5:width // 1] = 90
-    return image3
-
-def t1(image3):
-    shepp_t1 = np.zeros((width1, height1),dtype=np.uint8)
-    for i in range(width1):
-        for j in range(height1):
-            if image3[i][j] == 255:  ### white matter
-                shepp_t1[i][j] = 680//4
-            elif image3[i][j] == 150: ### gray matter
-                shepp_t1[i][j] = 810//9
-            elif image3[i][j] == 90:  ### fat
-                shepp_t1[i][j] =  240 //2
-            elif image3[i][j] == 0: ### water
-                shepp_t1[i][j] = 3000//100
-            else:
-                shepp_t1[i][j] = 100
-    return shepp_t1
-
-def t2(image3):
-    shepp_t2 = np.zeros((height1, width1),dtype=np.uint8)
-    for i in range(width1):
-        for j in range(height1):
-            if image3[i][j] == 255:  ### white matter
-                shepp_t2[i][j] = 90 // 1
-            elif image3[i][j] == 150:  ### gray matter
-                shepp_t2[i][j] = 100 // 5
-            elif image3[i][j] == 90:  ### fat
-                shepp_t2[i][j] = 85 //2
-            elif image3[i][j] == 0:  ### water
-                shepp_t2[i][j] = 3000 // 100
-            else:
-                shepp_t2[i][j] = 50
-    return shepp_t2
-
-def pd(image3):
-    shepp_pd = np.zeros((width1, height1),dtype=np.uint8)
-    for i in range(width1):
-        for j in range(height1):
-            if image3[i][j] == 255:  ### white matter
-                shepp_pd[i][j] = 500//10
-            elif image3[i][j] == 150: ### gray matter
-                shepp_pd[i][j] = 900//5
-            elif image3[i][j] == 90:  ### fat
-                shepp_pd[i][j] =  240 //3
-            elif image3[i][j] == 0: ### water
-                shepp_pd[i][j] = 3000//100
-            else:
-                shepp_pd[i][j] = 30
-    return shepp_pd
-
-def t1_t2_graph (i,j):
-    return t1_mat[i][j],t2_mat[i][j]
-
-height1, width1 = 512, 512 ####### change size from here
+    in_image = np.zeros((height, width),dtype=np.uint8)
+    in_image[height // 4:height // 2, width // 9:width // 3] = 150
+    in_image[height// 2:height // 1, width // 3:width // 2] = 255
+    in_image[height // 13:height // 5, width // 5:width // 1] = 90
+    return in_image
 
 
-image = create_phantom1(height1, width1)
+height1, width1 = 512,512
+
+image1 = create_phantom1(height1, width1)
 image2 = create_phantom2(height1, width1)
 image3 = create_phantom3(height1, width1)
-t1_mat = t1(image3)
-t2_mat= t2(image3)
-pd_mat= pd(image3)
 
-cv2.imwrite('image1.png', image)
+cv2.imwrite('image1.png', image1)
 cv2.imwrite('image2.png', image2)
 cv2.imwrite('image3.png', image3)
-# cv2.imwrite('image_t1.jpg', t1_mat)
-# cv2.imwrite('image_t2.jpg', t2_mat)
-# cv2.imwrite('image_pd.jpg', pd_mat)
--- generation_ready.py
+++ generation_ready.py
@@ -1,77 +1,73 @@
 import cv2
 import numpy as np
 
-
-def t1(image3):
-    shepp_t1 = np.zeros((image3.shape[0], image3.shape[1]), dtype=np.uint8)
-    for i in range(image3.shape[0]):
-        for j in range(image3.shape[1]):
-            if image3[i][j] == 255:  # white matter
-                shepp_t1[i][j] = 680//4
-            elif image3[i][j] == 150:  # gray matter
-                shepp_t1[i][j] = 810//9
-            elif image3[i][j] == 90:  # fat
-                shepp_t1[i][j] = 240 // 2
-            elif image3[i][j] == 0:  # water
-                shepp_t1[i][j] = 3000//100
+def t1(in_image):
+    shepp_t1 = np.zeros((in_image.shape[0],in_image.shape[1]),dtype=np.uint8)
+    for i in range(in_image.shape[0]):
+        for j in range(in_image.shape[1]):
+            if in_image[i][j] == 255:  ### white matter
+                shepp_t1[i][j] = 0
+            elif in_image[i][j] == 150:  ### gray matter
+                shepp_t1[i][j] = 70
+            elif in_image[i][j] == 90:  ### fat
+                shepp_t1[i][j] = 150
+            elif in_image[i][j] == 0:  ### water
+                shepp_t1[i][j] = 255
             else:
-                shepp_t1[i][j] = 3000 // 100
+                shepp_t1[i][j] = 30
     return shepp_t1
 
 
-def t2(image3):
-    shepp_t2 = np.zeros((image3.shape[0], image3.shape[1]))
-    for i in range(image3.shape[0]):
-        for j in range(image3.shape[1]):
-            if image3[i][j] == 255:  # white matter
-                shepp_t2[i][j] = 90 // 1
-            elif image3[i][j] == 150:  # gray matter
-                shepp_t2[i][j] = 100 // 5
-            elif image3[i][j] == 90:  # fat
-                shepp_t2[i][j] = 85 // 2
-            elif image3[i][j] == 0:  # water
-                shepp_t2[i][j] = 3000 // 100
+def t2(in_image):
+    shepp_t2 = np.zeros((in_image.shape[0], in_image.shape[1]),dtype=np.uint8)
+    for i in range(in_image.shape[0]):
+        for j in range(in_image.shape[1]):
+            if in_image[i][j] == 255:  ### white matter
+                shepp_t2[i][j] = 50
+            elif in_image[i][j] == 150:  ### gray matter
+                shepp_t2[i][j] = 255
+            elif in_image[i][j] == 90:  ### fat
+                shepp_t2[i][j] = 0
+            elif in_image[i][j] == 0:  ### water
+                shepp_t2[i][j] = 200
             else:
-                shepp_t2[i][j] = 3000 // 100
-
+                shepp_t2[i][j] = 100
     return shepp_t2
 
-
-def pd(image3):
-    shepp_pd = np.zeros((image3.shape[0], image3.shape[1]))
-    for i in range(image3.shape[0]):
-        for j in range(image3.shape[1]):
-            if image3[i][j] == 255:  # white matter
-                shepp_pd[i][j] = 500//10
-            elif image3[i][j] == 150:  # gray matter
-                shepp_pd[i][j] = 900//5
-            elif image3[i][j] == 90:  # fat
-                shepp_pd[i][j] = 240 // 3
-            elif image3[i][j] == 0:  # water
-                shepp_pd[i][j] = 3000//100
+def pd(in_image):
+    shepp_pd = np.zeros((in_image.shape[0], in_image.shape[1]),dtype=np.uint8)
+    for i in range(in_image.shape[0]):
+        for j in range(in_image.shape[1]):
+            if in_image[i][j] == 255:  # white matter
+                shepp_pd[i][j] = 50
+            elif in_image[i][j] == 150:  # gray matter
+                shepp_pd[i][j] = 140
+            elif in_image[i][j] == 90:  # fat
+                shepp_pd[i][j] = 120
+            elif in_image[i][j] == 0:  # water
+                shepp_pd[i][j] = 30
             else:
-                shepp_pd[i][j] = 3000 // 100
+                shepp_pd[i][j] = 90
 
     return shepp_pd
 
 
-def t1_t2_graph(i, j):
-    return t1_mat[i][j], t2_mat[i][j]
 
 
-def wheelEvent(self, event):
-    increaseFactor = 10
-    decreaseFactor = -10
+# def wheelEvent(self, event):
+#     increaseFactor = 10
+#     decreaseFactor = -10
+#
+#     if event.angleDelta().y() > 0:
+#         zoomFactor = increaseFactor
+#         for i in range(image.shape[0]):
+#             for j in range(image.shape[1]):
+#                 if image[i][j] < 245:
+#                     image[i][j] = image[i][j] + increaseFactor
+#     else:
+#         zoomFactor = decreaseFactor
+#         for i in range(image.shape[0]):
+#             for j in range(image.shape[1]):
+#                 if image[i][j] > 10:
+#                     image[i][j] = image[i][j] - decreaseFactor
 
-    if event.angleDelta().y() > 0:
-        zoomFactor = increaseFactor
-        for i in range(image.shape[0]):
-            for j in range(image.shape[1]):
-                if image[i][j] < 245:
-                    image[i][j] = image[i][j] + increaseFactor
-    else:
-        zoomFactor = decreaseFactor
-        for i in range(image.shape[0]):
-            for j in range(image.shape[1]):
-                if image[i][j] > 10:
-                    image[i][j] = image[i][j] - decreaseFactor
